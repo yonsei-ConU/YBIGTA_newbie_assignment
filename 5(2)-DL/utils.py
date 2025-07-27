295e6a9a5fbd6b01e92f7f86304fbaa185fb4e0a
yonsei-conu <jaeyeol.lee@gmail.com>
2025-07-27 13:13:13 +0900

    init 5.2 DL

---
+++ 5(2)-DL/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def accuracy(y_true, y_pred):
+    return np.average(y_true==y_pred)
+
+
+# Helper function to plot a decision boundary.
+def plot_decision_boundary(pred_func, train_data, color):
+    # Set min and max values and give it some padding
+    x_min, x_max = train_data[:, 0].min() - .5, train_data[:, 0].max() + .5
+    y_min, y_max = train_data[:, 1].min() - .5, train_data[:, 1].max() + .5
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole gid
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.RdYlGn)
+    plt.scatter(train_data[:, 0], train_data[:, 1], c=color, cmap=plt.cm.RdYlGn)
